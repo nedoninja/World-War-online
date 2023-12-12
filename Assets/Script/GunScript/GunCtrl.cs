a9fd88f238660cf4e07cc6eb03c3545b91674566
Svatoslav Semenov <semenovsvatoslav19@gmail.com>
2023-12-12 19:56:01 +0300

    naraborki

---
+++ Assets/Script/GunScript/GunCtrl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuCtrl : MonoBehaviour
+{
+    public List<Game
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
